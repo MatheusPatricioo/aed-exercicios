45aafcb9c4e72610ab29d1ceff3aba432065d254
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-02-17 16:42:49 -0300

    up

--- exercicio3.c
+++ exercicio3.c
@@ -3,9 +3,11 @@ int main()
     int var = 15;
     int *ptr;
 
-    ptr = &var; 
+    ptr = &var;
 
     printf("Conteudo de var = %d\n", var);
-     printf("endereco de var = %d\n", &var);
+    printf("endereco de var = %d\n", &var);
+    printf("endereco de var = %d\n", *ptr);
+    printf("endereco de var = %d\n", ptr);
     return 0;
 }
