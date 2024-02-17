100372f4226b650824a3ce0bda3f5398b213df44
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-02-17 17:23:23 -0300

    up

--- exercicio3.c
+++ exercicio3.c
@@ -9,9 +9,17 @@ int main()
     printf("endereco de var = %d\n", &var);
     printf("apontando para o conteudo de ptr = %d\n", *ptr);
     printf("endereco de ptr = %d\n", &ptr);
+
+    *ptr = 73;
+        printf("Conteudo de var = %d\n", var);
+    printf("endereco de var = %d\n", &var);
+    printf("apontando para o conteudo de ptr = %d\n", *ptr);
+    printf("endereco de ptr = %d\n", &ptr);
+
+
     return 0;
 }
 
 // sempre que usar "&", estou chamando o endereço daquela variavel.
-// sempre que usar "*", estou apontando para o conteudo daquela variavel.
-// ou seja, quando uso *ptr, to apontando para o conteudo da variavel que ptr aponta, neste caso é a variavel var
+// quando uso *ptr, to apontando para o conteudo da variavel que ptr aponta, neste caso é a variavel var
+// *ptr = O APONTADO POR...
