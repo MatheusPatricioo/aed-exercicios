8f1f481b6b0ba097169a59e735a232755e68116b
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-02-18 14:38:05 -0300

    Update exercicio4.c

--- exercicio4.c
+++ exercicio4.c
@@ -14,6 +14,11 @@ int main()
     /* faz com que o ponteiro va naquele endereço de
      memoria e puxe o VALOR. Ou seja, ele segue o endereço de memoria,
       mas mostra o valor que ta la e nao o próprio endereço*/
+
+          /* Quando o ponteiro P1, aponta pra variavel X, o endereço dessa variavel
+          vira o valor do ponteiro P1. Ou seja, embora o valor dele vire o endereço daquela
+          variavel, ele ainda tem seu proprio endereço*/
+
      return 0; 
     
 }
