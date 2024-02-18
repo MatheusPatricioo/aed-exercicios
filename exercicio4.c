0f13a5ade167fa1315f565478e72a3bb4ad9bbfa
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-02-18 13:41:55 -0300

    up

--- exercicio4.c
+++ exercicio4.c
@@ -3,7 +3,12 @@ int main()
     int a = 20;
     int *ptr;
 
-    
+    ptr = &a;
+
+    printf("Conteudo de a = %d\n", a);
+    printf("endereco de a = %d\n", & a);
+    printf("endereco de a, atraves do ponteiro = %d\n", ptr);
+    printf("conteudo de a, atraves do ponteiro = %d\n", *ptr);
     /* code */
     return 0;
 }
