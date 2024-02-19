59b43e18aeca1ff182cfd57b73e861c8ebbe2102
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-02-19 19:09:37 -0300

    up

--- exercicio8.c
+++ exercicio8.c
@@ -1,6 +1,7 @@
+/*
 #include <stdio.h>
 
-// Função que passa parâmetro por valor
+/ Função que passa parâmetro por valor
 void passarPorValor(int x) {
     x++;
     printf("Dentro da funcao: x = %d\n", x);
@@ -13,5 +14,24 @@ int main() {
     passarPorValor(num);
     printf("Depois da funcao: num = %d\n", num);
 
+    return 0;
+}
+*/
+#include <stdio.h>
+
+// Função que passa parâmetro por referência (ponteiro)
+void incrementaPorReferencia(int *x) {
+    (*x)++; // Incrementa o valor apontado por x
+    printf("Dentro da funcao: *x = %d\n", *x);
+}
+
+int main() {
+    int num = 10;
+    printf("Antes da funcao: num = %d\n", num);
+    
+    // Chamando a função que passa parâmetro por referência (ponteiro)
+    incrementaPorReferencia(&num);
+    
+    printf("Depois da funcao: num = %d\n", num);
     return 0;
 }
