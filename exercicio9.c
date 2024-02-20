220963534a85cf6acf0252df2a2d0320f44a20aa
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-02-20 12:47:56 -0300

    up

--- exercicio9.c
+++ exercicio9.c
@@ -3,7 +3,7 @@
 int main() {
     int vetor[5] = {0, 1, 2, 3, 4};
     for (int i = 0; i < 5; i++) {
-        printf("Posicao de memoria do elemento %d: %p\n", i, (void*)&vetor[i]);
+        printf("Posicao de memoria do elemento %d: %p\n", vetor[i],&vetor[i]);
     }
     return 0;
 }
