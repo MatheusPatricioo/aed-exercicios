509f0a66ef075698aa4f3ec38eada6c5184cc036
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-02-21 13:36:37 -0300

    Update exercicio9.c

--- exercicio9.c
+++ exercicio9.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int vetor[5] = {0, 1, 2, 3, 4};
-    for (int i = 0; i < 5; i++) {
+    int vetor[5] = {0, 1, 2, 3, 4, 5};
+    for (int i = 0; i < 6; i++) {
         printf("Posicao de memoria do elemento %d: %p\n", vetor[i],&vetor[i]);
     }
     return 0;
